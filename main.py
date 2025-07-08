a2fc41faa41beaf46d4a04ce2b080caa840cff77
Raunak <69701767+REVSIX@users.noreply.github.com>
2025-07-08 00:45:15 -0400

    Initial Commit (Drivebase Testing)

--- main.py
+++ main.py
@@ -5,11 +5,65 @@ from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
+
+# Constants
+wheel_diameter = 55.5
+axle_track = 104
+
+# Calculate the light threshold. Choose values based on your measurements.
+BLACK = 9
+WHITE = 85
+threshold = (BLACK + WHITE) / 2
+
+# Set the drive speed at 100 millimeters per second.
+DRIVE_SPEED = 100
+
+# Initialize the EV3 Brick
 ev3 = EV3Brick()
-ev3.screen.print("Hello World")
-wait(1000)
+
+# Motors & Drivebase Initialization
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-my_robot = DriveBase(left_motor, right_motor, 56, 128)
-#my_robot.distance_control.pid(100, 1, 10)
-my_robot.straight(100)
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+
+# Conversion Utility Functions
+def inch_to_mm(float num):
+    return num * 25.4 # Recommended Conversion Factor in PyBricks Docs
+
+# Modular Commands
+def drive(boolean move_forward):
+    if (move_forward):
+        return robot.straight(inch_to_mm(12))
+    else:
+        return robot.straight(inch_to_mm(-12))
+
+def turn_left():
+    return robot.turn(-90)
+
+def turn_right():
+    return robot.turn(90)
+
+for i in range(10):
+    drive(True)
+    
+# # Set the gain of the proportional line controller. This means that for every
+# # percentage point of light deviating from the threshold, we set the turn
+# # rate of the drivebase to 1.2 degrees per second.
+
+# # For example, if the light value deviates from the threshold by 10, the robot
+# # steers at 10*1.2 = 12 degrees per second.
+# PROPORTIONAL_GAIN = 1.2
+
+# # Start following the line endlessly.
+# while True:
+#     # Calculate the deviation from the threshold.
+#     deviation = line_sensor.reflection() - threshold
+
+#     # Calculate the turn rate.
+#     turn_rate = PROPORTIONAL_GAIN * deviation
+
+#     # Set the drive base speed and turn rate.
+#     robot.drive(DRIVE_SPEED, turn_rate)
+
+#     # You can wait for a short time or do other things in this loop.
+#     wait(10)
